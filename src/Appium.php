2b443549cb829fe0d1ae92fd9fad172d51b2f7c4
flo <hopemagie@gmail.com>
2018-09-28 23:25:23 +0200

    ios

--- src/Appium.php
+++ src/Appium.php
@@ -2,10 +2,33 @@
 
 namespace Codeception\Module;
 
-use Codeception\Module;
+use Codeception\Module as CodeceptionModule;
 
-class Appium extends Module
+class Appium extends CodeceptionModule
 {
+    protected $config = [
+        'host' => '0.0.0.0',
+        'local' => true,
+        'port' => 4723,
+        'browserName' => '',
+        'resetAfterSuite' => true,
+        'resetAfterCest' => false,
+        'resetAfterTest' => false,
+        'resetAfterStep' => false,
+        'desiredCapabilities' => array(
+            'platformName' => 'iOS',
+            'platformVersion' => '11.2',
+            'deviceName' => 'iPhone 8',
+            'xcodeOrgId' => '',
+            'xcodeSigningId' => 'iPhone Developer',
+            'noReset' => true,
+            'fullReset' => false,
+            'clearSystemFiles' => true,
+            'automationName' => 'XCUITest',
+            'bundleId' => 'com.openclassrooms.openclassrooms',
+            'showIOSLog' => false
+        )
+    ];
 
     public function greet($name)
     {
